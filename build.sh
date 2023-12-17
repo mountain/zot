5dfc2f3b78a4ae6e5f6ea6d5facf7b8f30496a97
Mingli Yuan <mountain@users.noreply.github.com>
2023-12-18 03:36:50 +0800

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-qjsc -flto -fno-string-normalize -fno-regexp -fno-json -fno-proxy -fno-map -fno-typedarray -fno-promise -fno-bigint -o zot -S 250000000 zot.js
+qjsc -flto -fno-eval -fno-string-normalize -fno-regexp -fno-json -fno-proxy -fno-map -fno-typedarray -fno-promise -fno-bigint -o zot -S 250000000 zot.js
